3f41170fbfd3368230fe19a517536e7ba162f16b
dimasm93 <software.dimas_m@icloud.com>
2020-01-12 22:10:17 +0700

    combine group function and single function

--- buildin-function/group-function.sql
+++ buildin-function/group-function.sql
@@ -1,8 +1,8 @@
 select  
-    sum(salary) as total_salary,
-    min(salary) as minimal_salary,
-    max(salary) as maximum_salary,
-    count(salary) as count_rows,
-    avg(salary) as average_salary
+    to_char(sum(salary), 'FM999,999') as total_salary,
+    to_char(min(salary),'FM999,999') as minimal_salary,
+    to_char(max(salary),'FM999,999') as maximum_salary,
+    to_char(count(salary),'FM999,999') as count_rows,
+    to_char(avg(salary),'FM999,999') as average_salary
 from employees
 where employee_id in (132,128 , 136, 127, 135, 140, 144, 119, 131, 182);
