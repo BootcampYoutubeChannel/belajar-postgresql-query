74739566842b0240a5311142cbd635a9d080f0ba
dimasm93 <software.dimas_m@icloud.com>
2020-01-15 21:29:08 +0700

    inner join

--- join-tables/inner-join.sql
+++ join-tables/inner-join.sql
@@ -16,3 +16,21 @@ from
     employees emp, departments dep 
 where emp.employee_id = dep.manager_id
 limit 10;
+
+select
+    d.department_id as kode_department,
+    d.department_name as nama_department,
+    d.location_id as department_location_id,
+    l.location_id as location_id,
+    l.street_address as alamat
+from 
+    locations l inner join departments d on (l.location_id = d.location_id);
+
+
+select
+    d.department_id as kode_department,
+    d.department_name as nama_department,
+    e.employee_id as kode_karyawan,
+    concat(e.first_name, ' ', e.last_name) as nama_karyawan
+from 
+    employees e inner join departments d on (e.department_id = d.department_id);
