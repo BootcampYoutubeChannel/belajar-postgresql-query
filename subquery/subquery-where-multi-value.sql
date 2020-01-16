f1f2ae663f622ecae2cce74e50c4ca6bc51b8660
dimasm93 <software.dimas_m@icloud.com>
2020-01-16 16:16:43 +0700

    subquery equal any

--- subquery/subquery-where-multi-value.sql
+++ subquery/subquery-where-multi-value.sql
@@ -5,5 +5,13 @@ select
 from 
     employees e
 where 
-    e.salary in (select j.max_salary from jobs j)
-limit 10;
+    e.salary in (select j.max_salary from jobs j);
+
+select
+    e.employee_id as nik,
+    e.first_name as nama,
+    e.salary as gaji_sebulan
+from 
+    employees e
+where 
+    e.salary = any (select j.max_salary from jobs j);
